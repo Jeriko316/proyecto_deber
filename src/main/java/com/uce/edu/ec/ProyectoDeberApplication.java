6875cbd00addf0d6d229dfa3331fc05656bed8dd
Jeriko316 <122418627+Jeriko316@users.noreply.github.com>
2023-11-12 19:24:37 -0500

    segundo cambio en la misma rama

--- src/main/java/com/uce/edu/ec/ProyectoDeberApplication.java
+++ src/main/java/com/uce/edu/ec/ProyectoDeberApplication.java
@@ -15,6 +15,7 @@ public class ProyectoDeberApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("primera ejecucion de la app");
+		System.out.println("segunda ejecucion de la app mismo branch");
 	}
 
 }
